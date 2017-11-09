ce635a26644becfa685b0c40bce1e5a19c47a4c3
Rishabh Bansal <rishabhbansal1855@gmail.com>
2017-11-09 22:39:31 +0530

    Update contact-form-suelo.php

--- assets/php/contact-form-suelo.php
+++ assets/php/contact-form-suelo.php
@@ -4,7 +4,7 @@
 	$email = trim($_POST['email']);
 	$message = trim($_POST['message']);
 	
-	$emailTo = 'naman@outlook.in'; //Put your own email address here
+	$emailTo = 'rishabhbansal1855@gmail.com'; //Put your own email address here
 	$subject = 'Question about Ja.';
 	$body = "Name: $name \n\nEmail: $email \n\nMessage:\n$message";
 	$headers = 'From: '.$email."\r\n" .
